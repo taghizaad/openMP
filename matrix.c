89ddb0bccbf1a57e70dd3b6a8eb8a55b8591f6f8
Hossein Taghizad <hossein.taghizad@gmail.com>
2021-07-06 07:09:04 +0430

    transpose_matrix() added

--- matrix.c
+++ matrix.c
@@ -50,7 +50,7 @@ float **add_matrix(float **mat1, float **mat2, int row1, int col1, int row2, int
 
 float **sub_matrix(float **mat1, float **mat2, int row1, int col1, int row2, int col2) {
     if (row1 != row2 || col1 != col2) {
-        fprintf(stderr, "incompatible matrix sizes for addition\n");
+        fprintf(stderr, "incompatible matrix sizes for subtraction\n");
         exit(EXIT_FAILURE);
     }
     float **result = create_empty_matrix(row1, col1);
@@ -73,69 +73,155 @@ float **mul_matrix(float **mat1, float **mat2, size_t row1, size_t col1, size_t 
     return result;
 }
 
+float **mul_matrix_scalar(float **mat1, float scalar, size_t row1, size_t col1) {
+    float **result = create_empty_matrix(row1, col1);
+    for (int i = 0; i < row1; ++i)
+        for (int j = 0; j < col1; ++j)
+            result[i][j] = mat1[i][j] * scalar;
+    return result;
+}
+
+float **transpose_matrix(float **mat, size_t row_v, size_t col_v, size_t *row_vT, size_t *col_vt){
+    *row_vT = col_v;
+    *col_vt = row_v;
+    float **result = create_empty_matrix(row_vT, col_vt);
+    for (int i = 0; i < row_v; ++i) {
+        for (int j = 0; j < col_v; ++j) {
+            result[j][i] = mat[i][j];
+        }
+    }
+    return result;
+}
+
+
 /**
  * this function calculate inverse of rank-1 update of matrix A.
  * The rank-1 update of A is shown by (A+uv^T)
  * If the rank-1 update of A is invertible, then it will be calculated by:
  * (A+(u)(v^T))^(-1) = (A^-1) + ((A^-1)(u)(v^T)(A^-1))/(1+(v^T)(A^-1)(u))
  * numerator: (A^-1)(u)(v^T)(A^-1)
- * denominator: (v^T)(A^-1)(u)
+ * denominator: 1 + (v^T)(A^-1)(u)
  * @param A is a K*K invertible matrix
  * @param u is K*1 vector
  * @param v is K*1 vector
  * @return
  */
-float *sherman_morrison(float *A_inv, float *u, float *v, int row_u, int col_u) {
+float **sherman_morrison(float **A_inv, float **u, float **v, size_t row_u, size_t col_u) {
 
-/*    float *p_denominator = mul_matrix(mul_matrix(v, A_inv, col_u, row_u, row_u), u, col_u, row_u, col_u);
-    float denominator = 1 + *p_denominator;
-    if (denominator == 0) {
-        return NULL;
-    }
-    float *p_numerator =
-            mul_matrix(mul_matrix(mul_matrix(A_inv, u, row_u, row_u, col_u), v, row_u, col_u, row_u), A_inv, row_u,
-                       row_u, row_u);
-    float *rank_one_update = sub_matrix(A_inv, p_numerator, row_u * row_u);
-    return rank_one_update;*/
+    float **result = create_empty_matrix(row_u, row_u);
+
+    float **num1 = mul_matrix(A_inv, u, row_u, row_u, row_u, col_u);
+    float **num2 = mul_matrix(v, A_inv, col_u, row_u, row_u, row_u);
+    float **numerator = mul_matrix(num1, num2, row_u, col_u, col_u, row_u);
+
+    float **den1 = mul_matrix(v, A_inv, col_u, row_u, row_u, row_u);
+    float **den2 = mul_matrix(den1, u, col_u, row_u, row_u, col_u);
+    float denominator = 1 + **den2;
+
+    float **second_term = mul_matrix_scalar(numerator, 1.0 / denominator, row_u, row_u);
+
+    result = add_matrix(A_inv, second_term, row_u, row_u, row_u, row_u);
+
+    return result;
 }
 
+float **read_matrix(size_t *rows, size_t *cols, const char *filename)
+{
+    if(rows == NULL || cols == NULL || filename == NULL)
+        return NULL;
+
+    *rows = 0;
+    *cols = 0;
+
+    FILE *fp = fopen(filename, "r");
+
+    if(fp == NULL)
+    {
+        fprintf(stderr, "could not open %s: %s\n", filename, strerror(errno));
+        return NULL;
+    }
+
+    float **matrix = NULL, **tmp;
+
+    char line[1024];
+
+    while(fgets(line, sizeof line, fp))
+    {
+        if(*cols == 0)
+        {
+            // determine the size of the columns based on
+            // the first row
+            char *scan = line;
+            int dummy;
+            int offset = 0;
+            while(sscanf(scan, "%d%n", &dummy, &offset) == 1)
+            {
+                scan += offset;
+                (*cols)++;
+            }
+        }
+
+        tmp = realloc(matrix, (*rows + 1) * sizeof *matrix);
+
+        if(tmp == NULL)
+        {
+            fclose(fp);
+            return matrix; // return all you've parsed so far
+        }
+
+        matrix = tmp;
+
+        matrix[*rows] = calloc(*cols, sizeof *matrix[*rows]);
+
+        if(matrix[*rows] == NULL)
+        {
+            fclose(fp);
+            if(*rows == 0) // failed in the first row, free everything
+            {
+                fclose(fp);
+                free(matrix);
+                return NULL;
+            }
+
+            return matrix; // return all you've parsed so far
+        }
+
+        int offset = 0;
+        char *scan = line;
+        for(size_t j = 0; j < *cols; ++j)
+        {
+            if(sscanf(scan, "%f%n", matrix[*rows] + j, &offset) == 1)
+                scan += offset;
+            else
+                matrix[*rows][j] = 0; // could not read, set cell to 0
+        }
+
+        // incrementing rows
+        (*rows)++;
+    }
+
+    fclose(fp);
+
+    return matrix;
+}
 
 /**
  * main method
  */
 void main() {
 
-/*    float *vector1;
-    float *vector2;
-    int rows = 2, cols = 3;
-    int vec_len = rows * cols;
-    vector1 = init_vector(rows, cols, 5);
-    vector2 = init_vector(rows, cols, 7);
-    if (vector1 != NULL && vector2 != NULL) {
-        float *pDouble = add_vector(vector1, neg_vector(vector2, vec_len), vec_len);
-        show_vector(pDouble, rows, cols);
+    size_t row_A_inv, col_A_inv, row_u, col_u, row_v, col_v, row_vT, col_vT;
+    float **A_inv = read_matrix(&row_A_inv, &col_A_inv, "A_inv.dat");
+    float **u = read_matrix(&row_u, &col_u, "u.dat");
+    float **v =read_matrix(&row_v, &col_v, "v.dat");
+    float **vT = transpose_matrix(v, row_v, col_v, &row_vT, &col_v);
+    show_matrix(vT, row_vT, col_vT);
 
-    }*/
+//    show_matrix(mul_matrix(u, v, row_u, col_u, row_v, col_v),row_u,col_v);
 
-/*
-    float *vec1, *vec2;
-    int row1 = 3, col1 = 1, col2 = 2;
-    vec1 = init_vector(row1, col1, 5);
-    vec2 = init_vector(col1, col2, 7);
-    show_vector(vec1, row1, col1);
-    printf("---------------\n");
-    show_vector(vec2, col1, col2);
-    printf("---------------\n");
-    float *mul_vec = mul_vector(vec1, vec2, row1, col1, col2);
-    show_vector(mul_vec, row1, col2);
-*/
-    float **mat1 = create_random_matrix(3, 7);
-    show_matrix(mat1, 3, 7);
-    Sleep(5000);
-    float **mat2 = create_random_matrix(7, 7);
-    show_matrix(mat2, 3, 7);
-    float **addMatrix = sub_matrix(mat1, mat2, 3, 7, 3, 7);
-    show_matrix(addMatrix, 3, 7);
+//    float **morrison = sherman_morrison(A_inv, u, v, 3, 1);
+//    show_matrix(morrison, 3, 3);
+
 
 
 }
