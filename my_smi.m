bba01b39c916b22aa44ed0ee5200d09ca49c5b1e
Hossein Taghizad <hossein.taghizad@gmail.com>
2022-01-09 16:28:32 -0500

    bases added

--- my_smi.m
+++ my_smi.m
@@ -7,32 +7,15 @@ x = inv(A0)*b;
 if M <= 0
     return
 end
-% cols = size(V,2);
-% P = randperm(cols);
-% U = U(:,P);
-% V= V(:,P);
-
 Y = inv(A0)*U;
 %(iii)
-count = 0;
 for l=1:M-1
     den = 1+V(:,l)'*Y(:,l);
-    l
-    Y
-     V(:,l)'*Y(:,l)
-%     V'*Y
-    if(den<1e-5)
-        count = count+1;
-    end
     x = x - Y(:,l) * ((V(:,l)'*x) / den);
     Y(:,l+1:M) = Y(:,l+1:M)- Y(:,l) * ((V(:,l)'*Y(:,l+1:M)) / den);
 end
 %(iv)
-l=M
+l=M;
 den = 1+V(:,l)'*Y(:,l);
- V(:,l)'*Y(:,l)
-if(den<1e-5)
-    count = count+1;
-end
 x = x - Y(:,l) * ((V(:,l)'*x) / den);
-count
+
--- my_test.m
+++ my_test.m
@@ -1,40 +1,66 @@
 
-% loadData;
+%  loadData;
 clc
-base1Ind = 1;
-base2Ind = 1;
-base1 = ttd(:,base1Ind);
-A01 = HH(base1Ind*numOfNodes-(numOfNodes-1):base1Ind*numOfNodes,:);
-A01inv = IH(base1Ind*numOfNodes-(numOfNodes-1):base1Ind*numOfNodes,:);
-base2 = ttd(:,base2Ind);
-A02 = HH(base2Ind*numOfNodes-(numOfNodes-1):base2Ind*numOfNodes,:);
-A02inv = IH(base2Ind*numOfNodes-(numOfNodes-1):base2Ind*numOfNodes,:);
+base = ones(numOfSwitches,1);
+for t=1:numOfSwitches-1
+    base(t+1) = 2^(t)+1;
+end
 
 b = ones(numOfNodes,1);
 
-difSM = zeros(1,len);
-difInv = zeros(1,len);
-difReg = zeros(1,len);
+start = 1;
+stop = len;
 
-start = 244765;
-stop = 244765;
+difSM = zeros(1,stop-start+1);
+% difInv = zeros(1,stop-start+1);
+% difReg = zeros(1,stop-start+1);
 
 tic
 for i=start:stop
     curttd = ttd(:,i);
-    if(sum(abs(curttd-base1) == 0) >= sum(abs(curttd-base2) == 0))
-        baseInd = base1Ind;
-        basettd = base1;
-        A0 = A01;
-        A0inv = A01inv;
-    else
-        baseInd = base2Ind;
-        basettd = base2;
-        A0 = A02;
-        A0inv = A02inv;
+    if base(1)<=i && i< base(2)
+        selectedBase = base(1);
+    elseif base(2)<=i && i< base(3)
+        selectedBase = base(2);
+    elseif base(3)<=i && i< base(4)
+        selectedBase = base(3);
+    elseif base(4)<=i && i< base(5)
+        selectedBase = base(4);
+    elseif base(5)<=i && i< base(6)
+        selectedBase = base(5);
+    elseif base(6)<=i && i< base(7)
+        selectedBase = base(6);
+    elseif base(7)<=i && i< base(8)
+        selectedBase = base(7);
+    elseif base(8)<=i && i< base(9)
+        selectedBase = base(8);
+    elseif base(9)<=i && i< base(10)
+        selectedBase = base(9);
+    elseif base(10)<=i && i< base(11)
+        selectedBase = base(10);
+    elseif base(11)<=i && i< base(12)
+        selectedBase = base(11);
+    elseif base(12)<=i && i< base(13)
+        selectedBase = base(12);
+    elseif base(13)<=i && i< base(14)
+        selectedBase = base(13);
+    elseif base(14)<=i && i< base(15)
+        selectedBase = base(14);
+    elseif base(15)<=i && i< base(16)
+        selectedBase = base(15);
+    elseif base(16)<=i && i< base(17)
+        selectedBase = base(16);
+    elseif base(17)<=i && i< base(18)
+        selectedBase = base(17);
+    else 
+        selectedBase = base(18);
     end
+
+    basettd = TT(:,selectedBase);
+    A0 = HH(selectedBase*numOfNodes-(numOfNodes-1):selectedBase*numOfNodes,:);
+
     %-----------------smwwd impl---------------------------
-    sw = find(TT(:,i) ~= TT(:,baseInd));
+    sw = find(TT(:,i) ~= TT(:,selectedBase));
     U = zeros(numOfNodes,length(sw));
     V = zeros(numOfNodes,length(sw));
     for j=1:length(sw)
@@ -52,6 +78,7 @@ for i=start:stop
     [x_ls,Y] = my_smi(b,U,V,A0);
     x = IH((i-1)*numOfNodes+1:i*numOfNodes,:)*b;
     difSM(i)=norm(x_ls-x);
+
     %     inverse = eye(numOfNodes);
     %     for j=size(V,2):-1:1
     %         inverse = inverse * (eye(numOfNodes) - (Y(:,j)*V(:,j)' / (1+V(:,j)'*Y(:,j))));
@@ -60,16 +87,16 @@ for i=start:stop
     %     difInv(i) = norm(inverse- IH((i-1)*numOfNodes+1:i*numOfNodes,:));
 
     %%% Regularization %%%
-    A = HH((i-1)*numOfNodes+1:i*numOfNodes,:);
-    g = A'*b;
-    [x_reg, info] = shermanMorrisonIteration(g,A,Z0);
-    difReg(i)=norm(x_reg-x);
+    %     A = HH((i-1)*numOfNodes+1:i*numOfNodes,:);
+    %     g = A'*b;
+    %     [x_reg, info] = shermanMorrisonIteration(g,A,Z0);
+    %     difReg(i)=norm(x_reg-x);
 
     %%%%%%%%%%%%%%%%%%%%%%
 end
 toc
 [valX,indX]=max(difSM)
-[valReg,indReg]=max(difReg);
+% [valReg,indReg]=max(difReg);
 % [valInv,indInv]=max(difInv);
 
 % close all
@@ -77,7 +104,7 @@ toc
 % axis = 1:len;
 % plot(axis,difSM)
 % title('norm(x - xSM)')
-% 
+%
 % subplot(2,1,2)
 % plot(axis,difReg)
 % title('norm(x - xReg)')
